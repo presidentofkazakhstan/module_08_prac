62869de40a7c06c6cc90993b1d090402ff054310
Тынышбаев Алибек Алмасулы <@corp.itstep>
2019-02-23 15:16:44 +0600

    prac module 8

---
+++ prac_module08/Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_module08
+{
+    public class Array
+    {
+        private int[] _array;
+        public int Size { get; set; }
+        public Array()
+        {
+            Size = 1;
+            _array = new int[Size];
+        }
+
+        public Array(int size)
+        {
+            Size = size;
+            _array = new int[Size];
+        }
+        public int this[int i]
+        {
+            get
+            {
+                return _array[i];
+            }
+            set
+            {
+                _array[i] = Convert.ToInt32(value * value);
+            }
+        }
+    }
+}
---
+++ prac_module08/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_module08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array(3);
+            Console.Write("Первый массив: ");
+            Console.WriteLine(array[0] = 1);
+            Console.Write("Второй массив: ");
+            Console.WriteLine(array[1] = 3);
+            Console.Write("Третий массив: ");
+            Console.WriteLine(array[2] = 5);
+            Console.WriteLine("Возведение в степень этих массивов");
+            Console.Write("1. ");
+            Console.Write("\n2. ");
+
+            for (int i = 0; i < array.Size; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
